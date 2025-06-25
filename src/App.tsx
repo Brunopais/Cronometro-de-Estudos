58ec2d6e9754c3b7f55c1ab16d640f88c160e67d
Brunopais <brunopais.oliveira@gmail.com>
2025-06-24 21:20:11 -0300

    Projeto cronometro do estudante

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Formulario from './components/Formulario';
+import Lista from './components/Lista';
+import { ITarefa } from './types/tarefa';
 
 function App() {
+  const [tarefas, setTarefas] = useState<ITarefa[]>([]);
+  const [tarefaSelecionada, setTarefaSelecionada] = useState<ITarefa | null>(null);
+
+  function selecionaTarefa(tarefa: ITarefa) {
+    setTarefaSelecionada(tarefa);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Formulario setTarefas={setTarefas} />
+      <Lista tarefas={tarefas} selecionaTarefa={selecionaTarefa} />
     </div>
   );
 }
